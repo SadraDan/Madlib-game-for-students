77b30549f914de40ba84f65f0b4308f23f59119f
Sadra Daneshmand <sadrathescientist@gmail.com>
2021-07-04 01:28:54 +0430

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,33 @@
+
+a_adjective = input("Enter an Adjective:")
+b_adjective = input("Enter Another Adjective:")
+bird        = input("Enter the Name of a Bird:")
+room        = input("Enter a Room in a House:")
+a_verb      = input("Enter a Past Verb:")
+b_verb      = input("Enter Another Verb:")
+a_name      = input("Enter a Relative's Name:")
+a_noun      = input("Enter a Noun:")
+liquid      = input("Enter a Liquid:")
+ing_verb    = input("Enter a Verb Ending in -ing:")
+body        = input("Enter a Body Part:")
+b_noun      = input("Enter a Plural Noun:")
+i2ng_verb   = input("Enter a Verb Ending in -ing:")
+b_noun      = input("Enter a Noun:")
+result       = input("Should we the Results?")
+
+def madlib_game():
+    print   ("It was a " + a_adjective +", cold November day.")
+
+    print   ("I woke up to the " + b_adjective + " smell of " + bird + " roasting in the " + room + " downstairs." )
+
+    print   ("I " + a_verb + " down the stairs to see if I could help " + b_verb + ".")
+
+    print   ("My mom said, \" see if " + a_name + " needs a fresh " + a_noun + ".\" ")
+
+    print   ("So I carried a tray of glasses full of " + liquid + " into the " + ing_verb + " room.")
+
+    print   ("When I got there, I couldn't believe my " + body + "!")
+
+    print   ("There were " + b_noun + " " + i2ng_verb + " on the " + b_noun + "!")
+
+madlib_game() 
